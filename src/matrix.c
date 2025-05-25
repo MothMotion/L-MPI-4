7e5428a1879094ebea13a0554e38707e574cb53b
Kali <kalimdenviroment@gmail.com>
2025-05-25 16:51:01 +0500

    optimized memory manegment

--- src/matrix.c
+++ src/matrix.c
@@ -7,37 +7,31 @@
 
 #include <mpi.h>
 
-uint8_t MatInit(arr_t** mat, const uint32_t size) { 
-  for(uint32_t i=0; i<size; ++i) {
-    mat[i] = malloc(size * sizeof(arr_t));
+uint8_t MatInitRect(arr_t** mat, const uint32_t size_x, const uint32_t size_y) { 
+  for(uint32_t i=0; i<size_x; ++i) {
+    mat[i] = malloc(size_y * sizeof(arr_t));
     if(!mat[i])
       return 0;
   }
   return 1;
 }
 
+uint8_t MatInitSqr(uint8_t** mat, const uint32_t size) {
+  return MatInitRect(mat, size, size);
+}
+
 void MatDeinit(arr_t** mat, const uint32_t size) {
-  for(uint32_t i=0; i<size; ++i)
+  for(uint32_t i=0; i<size; ++i) {
     free(mat[i]);
+    mat[i] = NULL;
+  }
   free(mat);
 }
 
-void MatPerformOper(arr_t** mat1, arr_t** mat2, arr_t** out, const uint32_t size,
+void MatPerformOper(arr_t** mat1, arr_t** mat2, arr_t** out, const uint32_t size_x, const uint32_t size_y,
                  void op(arr_t*, arr_t*, arr_t*, const uint32_t)) {
-  #ifdef SERIAL
-  for(uint32_t i=0; i<size; ++i)
-    op(mat1[i], mat2[i], out[i], size);
-
-  #else
-  int32_t rank, thr_size;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &thr_size);
-  uint32_t width = size / thr_size + (size%thr_size) ? 1 : 0;
-
-  for(uint32_t i = rank*width; i<size && i<((rank+1)*width); ++i)
-    op(mat1[i], mat2[i], out[i], size);
-
-  #endif
+  for(uint32_t i=0; i<size_x; ++i)
+    op(mat1[i], mat2[i], out[i], size_y);
 }
 
 void RandMat(arr_t** mat, const uint32_t size, const arr_t min_v, const arr_t max_v) {
